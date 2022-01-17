9a7ddec12c98af02ba61e457d5b4d9093dc664c4
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2022-01-17 17:36:19 +0300

    ш

--- main/Dict.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Dict {
-    public static void main(String[] args) throws Exception {
-      Console dictionaryConsole = new Console();
-      dictionaryConsole.start();
-
-    }
-}
--- start.bat
+++
@@ -1,4 +0,0 @@
-cd main
-javac *.java
-cd..
-java main.Dict
