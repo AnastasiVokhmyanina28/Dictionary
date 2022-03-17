e19521ea0c31acfe3fdb9a4b9d2a0c572f320fc0
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2022-03-17 16:29:45 +0300

    Dictionary

---
+++ src/main/java/controller/FileOperation.java
@@ -0,0 +1,105 @@
+package controller;
+import java.io.*;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import config.DictionaryType;
+
+public class FileOperation {
+    private String path;
+    private DictionaryType dictionaryType;
+
+    public FileOperation(String path, DictionaryType dictionaryType) {
+        this.dictionaryType = dictionaryType;
+        this.path = path;
+    }
+
+    public String fileReading() {
+        StringBuilder data = new StringBuilder();
+        try {
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                data.append(scanner.nextLine()).append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return data.toString();
+    }
+
+
+    public String fileAdd(String key, String value) {
+
+        if (keyCheck(key) && valueCheck(value)) {
+            BufferedWriter bufferedWriter = null;
+            try {
+                FileWriter fileWriter = new FileWriter(path, true);
+                bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write( key + DictionaryType.getSymbol() + value + "\n");
+                bufferedWriter.flush();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    bufferedWriter.close();
+                } catch (Exception e) {
+                }
+            }
+            return Dictionary.ADD_KEY;
+        } else {
+            return Dictionary.SIMILARITY_TO_THE_PATTERN;
+        }
+    }
+
+
+    private boolean keyCheck(String key) {
+        String patKey =  dictionaryType.getPatternKey();
+        return Pattern.matches(patKey, key);
+    }
+
+    private boolean valueCheck(String value) {
+        String patValue =  dictionaryType.getPatternValue();
+        return Pattern.matches(patValue, value);
+    }
+
+
+    public void fileRemove(String key) {
+        String[] readLines = fileReading().split("\n");
+        BufferedWriter bufferedWriter = null;
+        try {
+            FileWriter fileWriter = new FileWriter(path);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            for (int i = 0; i < readLines.length; i++) {
+                if (!key.equals(readLines[i].split(DictionaryType.getSymbol())[0])) {
+                    bufferedWriter.write(readLines[i] + "\n");
+                }
+            }
+            bufferedWriter.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedWriter.close();
+            } catch (Exception e) {
+            }
+        }
+
+    }
+
+
+
+
+    public String  search(String key) {
+        String[] stringsSearch = fileReading().split("\n");
+
+            for (int i = 0; i < stringsSearch.length; i++) {
+                if (key.equals(stringsSearch[i].split(DictionaryType.getSymbol())[0])) {
+                   return stringsSearch[i];
+                }
+            }
+
+    return Dictionary.NO_KEY;
+    }
+}
--- src/main/java/view/Console.java
+++ src/main/java/view/Console.java
@@ -1,8 +1,11 @@
 package view;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Scanner;
 import controller.Dictionary;
 import config.DictionaryType;
+import controller.FileOperation;
+import model.DictionaryStorage;
  
 
 public class Console {
@@ -22,24 +25,95 @@ public class Console {
     public static final String ADD_ENTRY = "\t4 Adding an entry";
     public static final String CHANGE_THE_DICTIONARY = "\t5 Change the dictionary";
     public static final String EXIT = "\t6 Logout";
+    public static final String SYSTEM = " Choose a system";
+    public static final String MAP_DICTIONARY = "\t1 Work with map";
+    public static final String FILE_DICTIONARY = "\t2 Work with file";
     private Scanner scanner;
     private Dictionary dictionary;
-    private Map<Integer, Dictionary> dictionaries;
+    private Map<Integer, Dictionary> mapDictionaries;
+    private Map<Integer, FileOperation> fileDictionaries;
+    private FileOperation fileOperation;
     private boolean isRunningConsole = false;
 
 
-    public Console(Map<Integer, Dictionary> dictionaries) {
-
+    public Console() {
         scanner = new Scanner(System.in, "windows-1251");
-        this.dictionaries = dictionaries;
+
     }
 
+    public int startp(){
+        System.out.println(SYSTEM);
+        System.out.println(MAP_DICTIONARY);
+        System.out.println(FILE_DICTIONARY);
+        System.out.println(SELECT + DictionaryType.getSymbol());
+        return scanner.nextInt();
+    }
+
+
+    private void startS(int choice){
+
+        switch (choice){
+            case 1:
+                Map<Integer, Dictionary> dictionaries = new HashMap<>();
+                for (DictionaryType dictionaryType : DictionaryType.values()) {
+                    dictionaries.put(dictionaryType.getNumber(), creation(dictionaryType));
+                }
+                 this.mapDictionaries = dictionaries;
+                break;
+
+            case 2:
+                Map<Integer, FileOperation> fileDictionaries = new HashMap<>();
+                for (DictionaryType dictionaryType : DictionaryType.values()) {
+                    fileDictionaries.put(dictionaryType.getNumber(), creation1(dictionaryType));
+                }
+                this.fileDictionaries = fileDictionaries;
+                break;
+            default:
+                System.out.println(NO_COMMAND);
+                break;
+
+        }
+          }
+
+
+
+
+    public static Dictionary creation(DictionaryType dictionaryType) {
+        Map<String, String> localMap = new HashMap<>();
+        String dictionaryPath = dictionaryType.getDictionaryPath();
+        var data = new DictionaryStorage(dictionaryType.getDictionaryPath(), localMap).getData();
+        return new Dictionary(dictionaryPath, dictionaryType, data);
+    }
+
+
+    public static FileOperation creation1(DictionaryType dictionaryType) {
+        String dictionaryPath = dictionaryType.getDictionaryPath();
+        return new FileOperation(dictionaryPath, dictionaryType);
+    }
+
+
+
+
+
     public void start() {
+        int choice = this.startp();
+        this.startS(choice);
+
         int dictionarySelection = this.menuChoiceDictionary();
-        chooseDictionary(dictionarySelection);
-        while (!isRunningConsole) {
-            int choiceOfActions = this.choiceOfAction();
-            this.choice(choiceOfActions);
+        if (mapDictionaries != null) {
+            chooseDictionary(dictionarySelection);
+            while (!isRunningConsole) {
+                int choiceOfActions = this.choiceOfAction();
+                this.choice(choiceOfActions);
+            }
+        } else {
+            systemSelection(dictionarySelection);
+            while (!isRunningConsole) {
+                int choiceOfActions = this.choiceOfAction();
+                this.fileOperation(choiceOfActions);
+
+            }
+
         }
     }
 
@@ -51,9 +125,15 @@ public class Console {
         return scanner.nextInt();
     }
 
-    private void chooseDictionary(int chosenAction) {
-        this.dictionary = dictionaries.get(chosenAction);
+    public void  systemSelection(int chosenAction ) {
+        this.fileOperation = fileDictionaries.get(chosenAction);
     }
+
+    private void chooseDictionary(int chosenAction) {
+        this.dictionary = mapDictionaries.get(chosenAction);
+    }
+
+
 
     public int choiceOfAction() {
         System.out.println(SELECT_THE_COMMAND + DictionaryType.getSymbol());
@@ -74,7 +154,6 @@ public class Console {
                 for (String key : dictionary.getLocalMap().keySet()) {
                     System.out.println(key + DictionaryType.getSymbol() + dictionary.getLocalMap().get(key));
                 }
-
                 break;
             case 2:
                 System.out.println(ENTER_KEY);
@@ -113,5 +192,41 @@ public class Console {
                 System.out.println(NO_COMMAND);
                 break;
         }
+    }
+
+    private void fileOperation(int chosenAction){
+
+        switch (chosenAction){
+            case 1:
+                System.out.println(fileOperation.fileReading());
+                break;
+            case 2:
+                System.out.println(ENTER_KEY);
+                String userKey = scanner.next();
+                fileOperation.fileRemove(userKey);
+                break;
+            case 3:
+                System.out.println(ENTER_KEY);
+                String usKey = scanner.next();
+                System.out.println(fileOperation.search(usKey));
+                break;
+            case 4:
+                System.out.println(ENTER_KEY);
+                String key = scanner.next();
+                System.out.println(ENTER_VALUE);
+                String value = scanner.next();
+               System.out.println(fileOperation.fileAdd(key, value));
+                break;
+            case 5:
+                int nextDictionary = menuChoiceDictionary();
+                systemSelection(nextDictionary);
+                break;
+            case 6:
+                this.isRunningConsole = true;
+                break;
+            default:
+                System.out.println(NO_COMMAND);
+        }
+
     }
 }
