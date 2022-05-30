6e2163c6544a004ac4147ec1c936c38aafd5b63a
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2022-05-30 18:12:44 +0300

    :)
    добавлен файл pom.xml
     исправлен файл .bat

--- src/main/java/com/config/DictionaryType.java
+++ src/main/java/com/config/DictionaryType.java
@@ -3,7 +3,7 @@ package com.config;
 
 public enum DictionaryType {
 
-    DICTIONARY_ONE(1, "^[a-zA-Z]{4}$", "[а-яёА-ЯЁ]+", "src/main/resources/Dictionary.txt"),
+    DICTIONARY_ONE(1, "^[a-zA-Z]{4}$", "[а-яёА-ЯЁ]+", "../resources/Dictionary.txt"),
     DICTIONARY_TWO(2, "^[0-9]{5}$", "[а-яёА-ЯЁ]+", "../resources/digitalDictionary.txt");
 
     private static final String splitChar = ":";
